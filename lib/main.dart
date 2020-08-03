eae251944d7db16a758e2e3e5921d06db590f463
jorgetinto <jpinogaray@gmail.com>
2020-08-03 18:08:34 -0400

    se finaliza seccion 6

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,10 @@
 // import 'package:custom_painter/src/pages/animaciones_page.dart';
 // import 'package:custom_painter/src/labs/circular_progress_page.dart';
 // import 'package:custom_painter/src/pages/graficas_circulares_page.dart';
-import 'package:custom_painter/src/pages/slideshow_page.dart';
+// import 'package:custom_painter/src/pages/slideshow_page.dart';
 //import 'package:custom_painter/src/reto/cuadrado_animado_page.dart';
 // import 'package:custom_painter/src/pages/headers_page.dart';
+import 'package:custom_painter/src/pages/pinterest_page.dart';
 import 'package:flutter/material.dart';
  
 void main() => runApp(MyApp());
@@ -14,7 +15,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Diseños App',
-      home: SlideshowPage()
+      home: PinterestPage()
     );
   }
 }
